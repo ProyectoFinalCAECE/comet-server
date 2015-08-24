37c5dec6cab5116aa5582e2a85a4a81c7087cd08
Juan Saucedo <jsaucedo.dev@gmail.com>
2015-08-24 13:02:05 -0300

    adding comments to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+/**
+
+ * Module dependencies
+
+ */
+
 var models  = require('../models');
 var passport = require('passport');
 var jwt = require('express-jwt');
 var express = require('express');
 var router  = express.Router();
-
-
 
 module.exports = router;
