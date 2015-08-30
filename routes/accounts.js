0cffd08dda3533abd41c9360c051afa570836e3a
jsaucedo <jsaucedo.dev@gmail.com>
2015-08-29 23:41:16 -0300

    se agrega metodo de renovacion de contraseña

--- routes/accounts.js
+++ routes/accounts.js
@@ -6,14 +6,12 @@
 
  */
 var accountService  = require('../services/accountService');
-
 var mailerService = require('../services/mailer');
-
 var models  = require('../models');
-
 var express = require('express');
-
 var router  = express.Router();
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'mySecretPassword', userProperty: 'payload'});
 
 /*
 * Sends password recovery token to user's email, if confirmed.
@@ -48,6 +46,35 @@ router.post('/password/recover', function(req, res){
     return res.status(400).json({ errors: { all: 'Please provide required fields.'}});
   } else {
     accountService.recoverPassword(res, req.body.token, req.body.newpassword);
+  }
+});
+
+/*
+* Allows password renewal for current logged user.
+* @oldpassword
+* @newpassword
+*/
+
+router.post('/password/renew', auth, function(req, res){
+
+  if(!req.body.oldpassword || !req.body.newpassword){
+    return res.status(400).json({ errors: { all: 'Please provide required fields.'}});
+  } else {
+    // look for current user's account
+    models.User.findById(parseInt(req.payload._id)).then(function(user) {
+        if (!user) {
+            return res.status(401).json({ message: 'there\'s no User with provided token.' });
+        }
+
+        if(user.validatePassword(req.body.oldpassword)){
+          user.setPassword(req.body.newpassword);
+          user.save();
+          return res.status(200).json({});
+        } else {
+          return res.status(403).json({ errors: { all: 'Provided parameters are incorrect.'}});
+        }
+
+    });
   }
 });
 
