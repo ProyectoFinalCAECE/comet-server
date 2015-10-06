a7bfa33610dea2316fc9b95686ea79c162a2da82
jsaucedo <jsaucedo.dev@gmail.com>
2015-10-06 01:26:07 -0300

    *bug fix: se retornan todos los canales publicos de un proyecto aunque el usuario loggeado no pertenezca a ellos (+ los privados a los que si)

--- services/channelService.js
+++ services/channelService.js
@@ -112,29 +112,52 @@ module.exports.getChannel = function(req, res, user) {
 */
 module.exports.getChannels = function(req, res, user) {
   var channels_to_be_returned = [];
-  user.getChannels({ where: ['"Channel"."state" != ? AND "Channel"."ProjectId" = ?', "B", req.primaryParams.project_id],
-                      order: [['createdAt', 'DESC']],
-                      include: [{ model: models.User}]}).then(function(channels){
-                        //creating response
-                        var x;
-                        for (x in channels) {
-                          //filtering channels user is not assigned anymore
+  user.getProjects({ where: ['"ProjectUser"."ProjectId" = ? AND "Project"."state" != ?', req.primaryParams.project_id, "B"] }).then(function(projects){
+    if (projects === undefined || projects.length === 0) {
+      return res.status(404).json({ errors: { all: 'No se puede encontrar ningun proyecto con el id provisto.'}});
+    }
 
-                          if(channels[x].ChannelUser.active === true) {
-                              channels_to_be_returned.push({
-                                id: channels[x].id,
-                                name: channels[x].name,
-                                description: channels[x].description,
-                                createdAt: channels[x].createdAt,
-                                type: channels[x].type,
-                                state: channels[x].state,
-                                members: getChannelMembers(channels[x].Users),
-                                integrations: []
-                              });
-                          }
-                        }
-
-                        return res.json(channels_to_be_returned);
+    if(projects[0].ProjectUser.active === false){
+      return res.status(403).json({ errors: { all: 'El usuario no puede acceder al proyecto solicitado.'}});
+    } else {
+      models.Channel.findAll({ where: ['"Channel"."ProjectId" = ? AND "Channel"."state" != ?', req.primaryParams.project_id, "B"],
+                              include: [{ model: models.User}],
+                              order: [['createdAt', 'DESC']]}).then(function(channels){
+        //creating response
+        var x;
+        for (x in channels) {
+          //filtering channels user is not assigned anymore
+          if(channels[x].type === 'S') {
+              channels_to_be_returned.push({
+                id: channels[x].id,
+                name: channels[x].name,
+                description: channels[x].description,
+                createdAt: channels[x].createdAt,
+                type: channels[x].type,
+                state: channels[x].state,
+                members: getChannelMembers(channels[x].Users),
+                integrations: []
+              });
+          } else {
+            console.log('channels[x].Users is: ' + JSON.stringify(channels[x].Users));
+            var channel_members_ids = getActiveUsersIds(channels[x].Users);
+            if((channel_members_ids.indexOf(user.id)) > -1){
+              channels_to_be_returned.push({
+                id: channels[x].id,
+                name: channels[x].name,
+                description: channels[x].description,
+                createdAt: channels[x].createdAt,
+                type: channels[x].type,
+                state: channels[x].state,
+                members: getChannelMembers(channels[x].Users),
+                integrations: []
+              });
+            }
+          }
+        }
+        return res.json(channels_to_be_returned);
+      });
+    }
   });
 };
 
@@ -358,4 +381,21 @@ function getUsersIds(projectUsers){
       }
     }
     return ids_to_be_returned;
+}
+
+/*
+*Given a set of Users, returns an array containing its ids if they're active for the channel.
+*
+*/
+function getActiveUsersIds(channel_members){
+  var ids_to_be_returned = [];
+  if(channel_members){
+    var y;
+    for(y in channel_members){
+      if(channel_members[y].ChannelUser.active === true){
+        ids_to_be_returned.push(channel_members[y].id);
+      }
+    }
+  }
+  return ids_to_be_returned;
 }
