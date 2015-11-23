f76c78c76eca85d88303cca78e401111dad5c674
jsaucedo <jsaucedo.dev@gmail.com>
2015-11-23 00:02:48 -0300

    endpoint de actualizacion de projectIntegration especifica

--- services/validators/integrationValidator.js
+++ services/validators/integrationValidator.js
@@ -39,6 +39,48 @@ module.exports.validUpdateProjectIntegration = function(req, res, next){
 
 /*
 *
+* Checks if provided parameters to update a specific XX_Integration for a certain Project are valid or returns an appropiate response.
+* @projectId
+* @projectIntegrationId
+* @channelId
+* @name
+* @token
+*
+*/
+module.exports.validUpdateInstanceOfProjectIntegration = function(req, res, next){
+  // validate input parameters
+  var errors = {};
+  var hasErrors = false;
+
+  if (!req.primaryParams.project_id)  {
+    errors.project_id = 'Por favor ingrese el id de proyecto.';
+    hasErrors = true;
+  }
+
+  if (!req.params.id)  {
+    errors.project_integration_id = 'Por favor ingrese el id de proyectointegracion.';
+    hasErrors = true;
+  }
+
+  if (!req.body.channelId) {
+    errors.channelId = 'Por favor ingresa el id del canal al que deseas asociar la integración.';
+    hasErrors = true;
+  }
+
+  if (!req.body.name) {
+    errors.name = 'Por favor ingresa el nombre con el cual van a figurar los mensajes de la integración.';
+    hasErrors = true;
+  }
+
+  if (hasErrors) {
+    return res.status(400).json({ errors: errors });
+  }
+
+  next();
+};
+
+/*
+*
 * Checks if provided parameters to create a specific XX_Integration for a certain Project are valid or returns an appropiate response.
 * @projectId
 * @projectIntegrationId
