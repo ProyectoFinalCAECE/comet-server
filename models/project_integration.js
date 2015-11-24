eef44223fed64a1c8a6dad0869b49fb4f451e318
jsaucedo <jsaucedo.dev@gmail.com>
2015-11-24 20:49:48 -0300

    modelo "trello_integration"

--- models/project_integration.js
+++ models/project_integration.js
@@ -21,6 +21,7 @@ module.exports = function(sequelize, DataTypes) {
         classMethods:{
           associate: function(models) {
             ProjectIntegration.hasMany(models.GithubIntegration);
+            ProjectIntegration.hasMany(models.TrelloIntegration);
           }
         },
         indexes:[{
