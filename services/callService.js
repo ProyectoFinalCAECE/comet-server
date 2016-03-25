a597dd7c281361aa34879334d1d6b1775fc733ac
jsaucedo <jsaucedo.dev@gmail.com>
2016-03-25 15:39:15 -0300

    Calls: agrego 'call members' al crear una llamada

--- services/callService.js
+++ services/callService.js
@@ -53,13 +53,46 @@ module.exports.createNewCall = function(project_id, channel_id, user, req_body, 
 
         //saving new call
         call.save().then(function() {
-              // SAVE MEMBERS
 
-              // SAVE MESSAGE
+              if(!req_body.members){
+                req_body.members = [];
+              }
 
-              result.code = 200;
-              result.message = call;
-              return callback(result);
+              //Adding current user to Call Members array.
+              req_body.members.push(user.id);
+
+              //Removing duplicates
+              req_body.members = req_body.members.filter( onlyUnique );
+
+              // Save Members
+              // (must do it this way in order to return members within the service response)
+              associateCallMembers(req_body.members, call.id, function(){
+
+                call.getMembers().then(function(members){
+                  if(!members){
+                    members = [];
+                  }
+
+                  var data = {
+                      id: call.id,
+                      summary: call.summary,
+                      startHour: call.startHour,
+                      endHour: call.endHour,
+                      createdAt: call.createdAt,
+                      updatedAt: call.updatedAt,
+                      ChannelId: call.ChannelId,
+                      OwnerId: call.UserId,
+                      members: members
+                  };
+
+                  // SAVE MESSAGE
+
+                  result.code = 200;
+                  result.message = data;
+                  return callback(result);
+
+                });
+              });
         });
       });
     }
@@ -118,3 +151,68 @@ module.exports.updateCall = function(project_id, channel_id, user, call_id, req_
     }
   });
 };
+
+/**
+ * Save Call Members
+ * @param  {array}   members
+ * @param  {integer}   call_id
+ * @param  {Function} callback
+ */
+function associateCallMembers(members, call_id, callback) {
+  if(members){
+    models.User.findAll({where: { id: { in: members } } }).then(function(users){
+      if(users && users.length > 0){
+        console.log('users is: ', users);
+        console.log('users.length is: ', users.length);
+        var x;
+        var call_users_to_create = [];
+
+        for(x in members){
+          console.log('in for, members[x] is: ', members[x]);
+          var user = findById(users, members[x]);
+          console.log('found user!: ', user);
+          if(user !== null){
+            // store new Call Member instance into array
+            call_users_to_create.push({ alias: user.alias, profilePicture: user.profilePicture, UserId: user.id, CallId: call_id });
+          }
+        }
+
+        if(call_users_to_create.length > 0){
+          console.log('call_users_to_create is: ', call_users_to_create);
+          models.CallMember.bulkCreate(call_users_to_create).then(function(){
+            return callback();
+          });
+        }else{
+          return callback();
+        }
+      } else {
+        return callback();
+      }
+    });
+  } else {
+    return callback();
+  }
+}
+
+/**
+ * Given a set of Users, returns the one that matches provided id.
+ * @param  {array} users
+ * @param  {integer} user_id
+ * @return {User}
+ */
+function findById(users, user_id) {
+  var x;
+  for(x in users){
+    if(users[x].id === user_id){
+      return users[x];
+    }
+  }
+  return null;
+}
+
+/**
+ * Removes duplicates from provided array
+ */
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
