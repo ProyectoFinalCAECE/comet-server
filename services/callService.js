1b0f47fbf3298cf4504fa2651ad3469e2e7de9d8
jsaucedo <jsaucedo.dev@gmail.com>
2016-03-26 12:14:14 -0300

    Calls: sobreescribo miembros de videollamada y actualizo metodo 'update'

--- services/callService.js
+++ services/callService.js
@@ -160,12 +160,52 @@ module.exports.updateCall = function(project_id, channel_id, user, call_id, req_
           calls[0].save().then(function(){
             // UPDATE MEMBERS  -- > REPLACE OLD MEMBERS WITH NEW ONES
 
-            // UPDATE MESSAGE
+            if(!req_body.members){
+              req_body.members = [];
+            }
 
-            result.code = 200;
-            result.message = calls[0];
-            return callback(result);
+            //Adding current user to Call Members array.
+            req_body.members.push(user.id);
 
+            //Removing duplicates
+            req_body.members = req_body.members.filter( onlyUnique );
+
+            // Save Members
+            // (must do it this way in order to return members within the service response)
+            associateCallMembers(req_body.members, calls[0].id, function() {
+
+              calls[0].getMembers().then(function(members){
+                if(!members){
+                  members = [];
+                }
+
+                var data = {
+                    id: calls[0].id,
+                    summary: calls[0].summary,
+                    startHour: calls[0].startHour,
+                    endHour: calls[0].endHour,
+                    createdAt: calls[0].createdAt,
+                    updatedAt: calls[0].updatedAt,
+                    ChannelId: calls[0].ChannelId,
+                    OwnerId: calls[0].UserId,
+                    members: members
+                };
+
+                var data_to_store = {
+                  id: data.id,
+                  summary: data.summary,
+                  ChannelId: data.ChannelId
+                };
+
+                // UPDATE MESSAGE
+                // Updating message
+              //  messagingService.updateVideocallMessage(JSON.stringify(data_to_store), calls[0].ChannelId, calls[0].UserId);
+
+                result.code = 200;
+                result.message = calls[0];
+                return callback(result);
+              });
+            });
           });
         });
       });
@@ -181,40 +221,66 @@ module.exports.updateCall = function(project_id, channel_id, user, call_id, req_
  */
 function associateCallMembers(members, call_id, callback) {
   if(members){
-    models.User.findAll({where: { id: { in: members } } }).then(function(users){
-      if(users && users.length > 0){
-        console.log('users is: ', users);
-        console.log('users.length is: ', users.length);
-        var x;
-        var call_users_to_create = [];
+    removePreexistentMembers(call_id, function(){
+      models.User.findAll({where: { id: { in: members } } }).then(function(users){
+        if(users && users.length > 0){
+          console.log('users is: ', users);
+          console.log('users.length is: ', users.length);
+          var x;
+          var call_users_to_create = [];
 
-        for(x in members){
-          console.log('in for, members[x] is: ', members[x]);
-          var user = findById(users, members[x]);
-          console.log('found user!: ', user);
-          if(user !== null){
-            // store new Call Member instance into array
-            call_users_to_create.push({ alias: user.alias, profilePicture: user.profilePicture, UserId: user.id, CallId: call_id });
+          for(x in members){
+            console.log('in for, members[x] is: ', members[x]);
+            var user = findById(users, members[x]);
+            console.log('found user!: ', user);
+            if(user !== null){
+              // store new Call Member instance into array
+              call_users_to_create.push({ alias: user.alias, profilePicture: user.profilePicture, UserId: user.id, CallId: call_id });
+            }
           }
-        }
 
-        if(call_users_to_create.length > 0){
-          console.log('call_users_to_create is: ', call_users_to_create);
-          models.CallMember.bulkCreate(call_users_to_create).then(function(){
+          if(call_users_to_create.length > 0){
+            console.log('call_users_to_create is: ', call_users_to_create);
+            models.CallMember.bulkCreate(call_users_to_create).then(function(){
+              return callback();
+            });
+          }else{
             return callback();
-          });
-        }else{
+          }
+        } else {
           return callback();
         }
-      } else {
-        return callback();
-      }
+      });
     });
   } else {
     return callback();
   }
 }
 
+/**
+ * Given a call id, retrieves the Call object and removes the members it could have.
+ * @param  {[type]}   call_id  [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function removePreexistentMembers(call_id, callback) {
+  models.Call.findById(call_id).then(function(call){
+    if(call){
+      call.getMembers().then(function(members){
+        if(members){
+          var x;
+          for(x in members){
+            members[x].destroy();
+          }
+        } else {
+          return callback();
+        }
+      });
+    } else {
+      return callback();
+    }
+  });
+}
 /**
  * Given a set of Users, returns the one that matches provided id.
  * @param  {array} users
