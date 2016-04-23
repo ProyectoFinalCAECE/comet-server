51f48890b5d9618c1e1f9b205e89be70b7e4ccba
gascoj <juliogasco@gmail.com>
2016-04-23 10:53:07 -0300

    Modificacion de start.sh para que inicie el signalmaster

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,11 @@ echo '1 - starting redis-server'
 redis-server&
 echo 'redis-server started'
 
-echo '2 - starting nodemon'
+echo '2 - starting signalmaster'
+cd ../comet-signalmaster/ && node server.js&
+echo 'signalmaster started'
+
+echo '3 - starting nodemon'
 NODE_ENV=development nodemon ./bin/www
 echo 'nodemon started'
+
