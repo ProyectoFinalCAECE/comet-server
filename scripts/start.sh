da9b5c3cad1c94daab9e5d3f35fa7d7db5bd15da
marianoquevedo <marianorquevedo@gmail.com>
2016-05-14 16:41:25 -0300

    Modifico script de inicio
    Modifico el script de inicio (start.sh) para que reciba el NODE_ENV por parametro. Toma como valor default 'development'

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# default node environment
+# 1 is the first parameter passed to this script
+environment=${1:-development}
+
 echo "   ______                     __                                   ";
 echo "  / ____/___  ____ ___  ___  / /_   ________  ______   _____  _____";
 echo " / /   / __ \/ __ \`__ \/ _ \/ __/  / ___/ _ \/ ___/ | / / _ \/ ___/";
@@ -15,14 +19,5 @@ echo '2 - starting signalmaster'
 cd ../comet-signalmaster/ && node server.js&
 echo 'signalmaster started'
 
-echo '3 - starting nodemon'
-NODE_ENV=development nodemon ./bin/www
-echo "   ______                     __                                   ";
-echo "  / ____/___  ____ ___  ___  / /_   ________  ______   _____  _____";
-echo " / /   / __ \/ __ \`__ \/ _ \/ __/  / ___/ _ \/ ___/ | / / _ \/ ___/";
-echo "/ /___/ /_/ / / / / / /  __/ /_   (__  )  __/ /   | |/ /  __/ /    ";
-echo "\____/\____/_/ /_/ /_/\___/\__/  /____/\___/_/    |___/\___/_/     ";
-echo "                                                                   ";                                                                     
-
-
-echo 'nodemon started'
+echo '3 - starting nodemon -' $environment
+NODE_ENV=$environment nodemon ./bin/www
